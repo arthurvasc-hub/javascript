90181810b43da3d48d2f2848080554a05f0a5508
arthurvasc-hub <arthurvasc2@gmail.com>
2023-11-28 11:31:01 -0300

    JS exercicios

--- ex06/script.js
+++ ex06/script.js
@@ -1,12 +1,14 @@
 //condicionais//
 
-let idade = 18;
+let idade = prompt("Qual é a sua idade?");if (idade >= 18) { console.log("Você é maior de idade.");} else { console.log("Você é menor de idade.");}
 
-if (idade > 17) {
-    console.log("Você é maior de idade.");
-} else {
-    console.log("Você é MENOR de idade")
-}
+let numero = prompt("Digite um número:");if (numero % 2 === 0) { console.log("O número é par.");} else { console.log("O número é ímpar.");}
+
+let preco = prompt("Qual é o preço do item?");if (preco <= 50) { console.log("Você deve comprar este item!");} else { console.log("Este item está muito caro. Talvez você deva procurar outra opção.");}
+
+let num = prompt("Digite um número:");if (num > 0) { console.log("O número é positivo.");} else if (num < 0) { console.log("O número é negativo.");} else { console.log("O número é zero.");}
+
+let senha = prompt("Digite sua senha:");if (senha === "12345") { console.log("Acesso permitido!");} else { console.log("Acesso negado. Senha incorreta.");}
 
 // <
 // >
