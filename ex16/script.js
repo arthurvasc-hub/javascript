b8984f3a7a1d5ba0aaa812b35480c3314eb5d7a2
arthurvasc-hub <arthurvasc2@gmail.com>
2023-12-09 14:02:22 -0300

    js

---
+++ ex16/script.js
@@ -0,0 +1,17 @@
+/*function somar(x, y){
+    return x + y;
+}
+
+ARROw FUNCTION
+
+() => x + y;
+
+*/
+
+const somar = (x, y) => x + y;
+
+console.log(somar (10, 5));
+
+const sobrenome = sob => 'Bonieky ' + sob;
+
+console.log(sobrenome('Lacerda'));
