7bb7c0cfc7baddbe7f849bbd0a80b31c70182bf7
arthurvasc-hub <arthurvasc2@gmail.com>
2024-01-08 18:16:23 -0300

    estudos

--- ex21(obj)/script.js
+++ ex21(obj)/script.js
@@ -1,16 +1,22 @@
 let personagem = {
     nome: 'Arthur',
     idade: 24,
-    pais: 'Brasil',
-    profissao: ['alquimia', 'coletor', 'minerador'],
-
-    status: {
-    forca: 20,
-    inteligencia: 5,
-    stamina:15
-    }
+    carros: [
+        {modelo: 'Fiat', cor: 'preto'},
+        {modelo: 'Ferrari', cor: 'vermelho'}
+    ]
+    
 }
 
-console.log(`${personagem.nome} tem ${personagem.idade} anos.`)
-console.log(`e possui ${personagem.status.forca} de status STR `);
-console.log(personagem.profissao[1]);
+console.log(personagem.carros[1].modelo);
+
+//console.log(`${personagem.nome} tem ${personagem.idade} anos.`)
+//console.log(`e possui ${personagem.status.forca} de status STR `);
+//console.log(personagem.profissao[1]);
+
+//personagem.nome = 'Bola';
+//personagem.status.forca += 5;
+
+//personagem.profissao.push('Pescador')
+
+//console.log(personagem.profissao);
