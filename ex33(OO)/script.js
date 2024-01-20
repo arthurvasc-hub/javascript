39c0b9ea694fb40a603d04baafbaeae6d1163087
arthurvasc-hub <arthurvasc2@gmail.com>
2024-01-19 21:22:49 -0300

    estudos

---
+++ ex33(OO)/script.js
@@ -0,0 +1,11 @@
+class Person {
+constructor(name, age){
+    this.nome = name;
+    this.idade = age;
+}
+}
+
+let p1 = new Person('Arthur', 24);
+let p2 = new Person('Bieca', 27);
+let p3 = new Person('Misso', 27);
+
