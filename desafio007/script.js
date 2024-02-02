1fc5cf4886d49e991199985683a876c191787904
arthurvasc-hub <arthurvasc2@gmail.com>
2024-02-02 18:32:56 -0300

    estudos

--- desafio007/script.js
+++ desafio007/script.js
@@ -1,7 +1,9 @@
 let = char = new Archer('Tutu')
 
-console.log(char.name);
-console.log(char.life);
-console.log(`O arqueiro ${char.name} possui ${char.life} de vida, ${char.attack} de ataque e ${char.defense} de defesa`)
+console.log(`O tutuzin é sinistro, vulgo ${char.name} SINISTRO NO DOTA. ELe tem ${char.attack} de força e ${char.life} de vida `)
 
+
+let = char = new Knight('Bieca')
+
+console.log(`O grande ${char.name} é muito fraquinho no dota. Ele tem ${char.attack} de força e ${char.life} de vida `)
 
